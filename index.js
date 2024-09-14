2865771090554d9fe2a507a2388924e5d614d922
Hassaan200 <hassaankhaliq45@gmail.com>
2024-09-14 17:07:58 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,130 @@
+// first question's answer
+
+var bill = 1000;
+var unit = prompt("Enter your units");
+if (unit > 100 && unit <= 200) {
+    console.log("Your Bill is " + (bill + (unit - 100) * 5))
+}
+else if (unit > 200) {
+    console.log("Your Bill is " + (bill + (unit - 100) * 10))
+}
+else {
+    console.log("Your Bill is " + bill);
+}  
+
+// second question's answer
+
+// var per = prompt("Enter your Percentage");
+// if (per < 25){
+//     console.log("Your Garde is D")
+// }
+// else if(per > 25 && per < 45){
+//     console.log("Your Garde is C")
+// }
+// else if(per > 45 && per < 50){
+//     console.log("Your Garde is B")
+// }
+// else if(per > 50 && per < 60){
+//     console.log("Your Garde is B+")
+// }
+// else if(per > 60 && per < 80){
+//     console.log("Your Garde is A")
+// }
+// else{
+//     console.log("Your Garde is A+")
+// }
+
+// Third question's answer 
+
+// var i = 3, j = 5, k = 7;
+// if (i > j && j > k) {
+//     if(j < k){
+//         i = j
+//     }
+//     else{
+//         j = k
+//     }
+// }
+
+// else if (j > k) {
+//     j = i
+// }
+// else {
+//     i = k
+// }
+// console.log(i, j, k);
+
+// var i = 8, j = 15, k = 12;
+// if (i > j && j > k) {
+//     if(j < k){
+//         i = j
+//     }
+//     else{
+//         j = k
+//     }
+// }
+
+// else if (j > k) {
+//     j = i
+// }
+// else {
+//     i = k
+// }
+// console.log(i, j, k);
+
+// var i = 13, j = 15, k = 13;
+// if (i > j && j > k) {
+//     if(j < k){
+//         i = j
+//     }
+//     else{
+//         j = k
+//     }
+// }
+
+// else if (j > k) {
+//     j = i
+// }
+// else {
+//     i = k
+// }
+// console.log(i, j, k);
+
+// var i = 3, j = 5, k = 17;
+// if (i > j && j > k) {
+//     if(j < k){
+//         i = j
+//     }
+//     else{
+//         j = k
+//     }
+// }
+
+// else if (j > k) {
+//     j = i
+// }
+// else {
+//     i = k
+// }
+// console.log(i, j, k);
+
+// var i = 25, j = 15, k = 17;
+// if (i > j && j > k) {
+//     if(j < k){
+//         i = j
+//     }
+//     else{
+//         j = k
+//     }
+// }
+
+// else if (j > k) {
+//     j = i
+// }
+// else {
+//     i = k
+// }
+// console.log(i, j, k);
+
+
+
